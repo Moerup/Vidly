4abb3bf2dce7faddd60da26d00e9afbe66fa8506
Nikolaj Mørup <nikolajmorup@gmail.com>
2019-03-10 00:38:44 +0100

    Added forms to create Customers + Movies

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using  System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
@@ -24,6 +25,56 @@ namespace Vidly.Controllers
             var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);    
+        }
+
+        public ActionResult New()
+        {
+            var genre = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genre
+            };
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.Genre = movie.Genre;
+                movieInDb.NumberInStock = movie.NumberInStock;
+
+
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
 
         public ActionResult Details(int id)
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,16 +1,27 @@
 ﻿
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         public Genre Genre { get; set; }
+
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
+
         public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Number In Stock")]
         public byte NumberInStock { get; set; }
     }
 }
